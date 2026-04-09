1e1d123db6ccd91ce9af3a7175834099a68f6cd0
DIOGO SILVA LANA <202451060708@alunos.newtonpaiva.edu.br>
2026-04-08 22:17:13 -0300

    feat: adiciona lista de sandubas

---
+++ routesHamburgeria/data/arrayProdutos.ts
@@ -0,0 +1,60 @@
+import { prodType } from "../types/prodType";
+
+export const sandubas: prodType[] = [
+  {
+    id: "1",
+    nome: "Double X",
+    descricao: "blend de carne, queijo americano, picles e mostarda.",
+    preco: "R$58.90",
+    img: require("../assets/images/1.png")
+  },
+  {
+    id: "2",
+    nome: "Simple X",
+    descricao: "dois blends, cheddar derretido, cebola roxa e ketchup artesanal.",
+    preco: "R$58.90",
+    img: require("../assets/images/2.png")
+  },
+  {
+    id: "3",
+    nome: "Expert",
+    descricao: "blend, queijo prato, abacaxi grelhado e molho teriyaki.",
+    preco: "R$58.90",
+    img: require("../assets/images/3.png")
+  },
+  {
+    id: "4",
+    nome: "Barber D",
+    descricao: "costela desfiada, bacon crocante, cebola caramelizada e molho barbecue.",
+    preco: "R$58.90",
+    img: require("../assets/images/4.png")
+  },
+  {
+    id: "5",
+    nome: "Double XZ",
+    descricao: "blend angus, queijo brie, cogumelos salteados e maionese de trufas.",
+    preco: "R$58.90",
+    img: require("../assets/images/5.png")
+  },
+  {
+    id: "6",
+    nome: "Simple Z",
+    descricao: "burger de grão-de-bico, rúcula, tomate seco e homus.",
+    preco: "R$58.90",
+    img: require("../assets/images/7.png")
+  },
+  {
+    id: "7",
+    nome: "Simple P",
+    descricao: "dois blends, cheddar derretido, cebola roxa e ketchup artesanal.",
+    preco: "R$58.90",
+    img: require("../assets/images/8.png")
+  },
+  {
+    id: "8",
+    nome: "Clamber two",
+    descricao: "frango empanado, alface, tomate e maionese de alho.",
+    preco: "R$58.90",
+    img: require("../assets/images/9.png")
+  }
+];
